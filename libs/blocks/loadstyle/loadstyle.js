5520e147fda9831ed9d4defc996324853717c000
Okan Sahin <osahin@adobe.com>
2024-06-03 11:39:31 +0200

    add use-case

--- libs/blocks/loadstyle/loadstyle.js
+++ libs/blocks/loadstyle/loadstyle.js
@@ -1,8 +1,7 @@
-import { loadStyle, getConfig } from '../../utils/utils.js';
+import { loadStyle } from '../../utils/utils.js';
 
 export default function init() {
-  const { miloLibs } = getConfig();
-  loadStyle('https://metadata-differ--milo--mokimo.aem.page/blocks/loadstyle/external.css');
+  loadStyle('https://metadata-differ--milo--mokimo.aem.page/libs/blocks/loadstyle/external.css');
   document.getElementsByClassName('loadstyle')[0].innerHTML = 'this will get styled by loadStyle. ';
   console.log('el');
 }
