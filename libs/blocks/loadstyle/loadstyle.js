70e5f0047962f18c3101bf2006d38ac32b42edb5
Okan Sahin <osahin@adobe.com>
2024-06-03 11:38:38 +0200

    load external css sheeet

--- libs/blocks/loadstyle/loadstyle.js
+++ libs/blocks/loadstyle/loadstyle.js
@@ -2,7 +2,7 @@ import { loadStyle, getConfig } from '../../utils/utils.js';
 
 export default function init() {
   const { miloLibs } = getConfig();
-  loadStyle(`${miloLibs}/blocks/loadstyle/external.css`);
+  loadStyle('https://metadata-differ--milo--mokimo.aem.page/blocks/loadstyle/external.css');
   document.getElementsByClassName('loadstyle')[0].innerHTML = 'this will get styled by loadStyle. ';
   console.log('el');
 }
