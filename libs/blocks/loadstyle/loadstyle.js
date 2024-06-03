9af04f9b08e5a207ed731c98054fbf76e6265033
Okan Sahin <osahin@adobe.com>
2024-06-03 11:37:41 +0200

    loadstyle

--- libs/blocks/loadstyle/loadstyle.js
+++ libs/blocks/loadstyle/loadstyle.js
@@ -1,6 +1,8 @@
-import { loadStyle } from '../../utils/utils';
+import { loadStyle, getConfig } from '../../utils/utils.js';
 
-export default function init(el) {
-  el.innerHTML = 'this will get styled by loadStyle but too late. ';
+export default function init() {
+  const { miloLibs } = getConfig();
+  loadStyle(`${miloLibs}/blocks/loadstyle/external.css`);
+  document.getElementsByClassName('loadstyle')[0].innerHTML = 'this will get styled by loadStyle. ';
   console.log('el');
 }
